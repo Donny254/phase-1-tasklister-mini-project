908b9b1eecae99b13d10c88be15689a19d25df6d
don <donalduck254@gmail.com>
2025-12-28 09:20:29 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#task');
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  event.preventDefault();
+
+
+  const input = document.querySelector('#new-task-description')
+  const task = input.value.trim();
+
+
+  if (task !== '') {
+
+    buildToDoList();
+    input.value = '';
+  }
 });
+
+
+function buildToDoList(task) {
+  const li = document.createElement('li');
+  li.textContent = li;
+  taskList.appendChild(li);
+}
